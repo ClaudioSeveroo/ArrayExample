9e8834d0d555922218218f2fdb77e4672ce1dadd
“Claudio <claudiorobertosevero67@gmail.com>
2025-02-24 15:44:31 -0300

    master

---
+++ src/exemploarray/ExemploArray.java
@@ -0,0 +1,16 @@
+
+package exemploarray;
+
+public class ExemploArray {
+
+    public static void main(String[] args) {
+         /*Crie e inicialize um vetor de string com os dias da semana.*/
+        String[] semana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+        System.out.println("Dias da Semana:");
+        for (int i = 0; i < semana.length; i++) {
+            System.out.println(semana[i]);
+
+        }
+    }
+    
+}
